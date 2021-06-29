c53f76b1b0d64932586f6f511a10ff7cfc5b50e9
crackerFactory64 <lee.mander@gmail.com>
2021-06-29 16:59:57 +0100

    mobile nav done

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("burger").onclick = () => {
+  document.getElementById("mobile-nav").classList.toggle("hide");
+  document.getElementById("burger").classList.toggle("dim");
+};
